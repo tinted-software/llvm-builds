c6fcdf78bf576b9a0761ed01340d0f9b5e85cb83
Theo Paris <theo@theoparis.com>
2026-02-10 20:34:21 -0800

    feat: shared dylib build

--- build.sh
+++ build.sh
@@ -2,11 +2,13 @@
 set -eu
 
 cmake -B build -G Ninja \
+	-DCMAKE_C_COMPILER=clang \
+	-DCMAKE_CXX_COMPILER=clang++ \
+	-DCMAKE_ASM_COMPILER=clang \
 	-S llvm-project/llvm \
 	-DLIBCLC_CUSTOM_LLVM_TOOLS_BINARY_DIR=$PWD/build/bin \
-	-DBUILD_SHARED_LIBS=OFF \
 	-DCMAKE_INSTALL_PREFIX=$PWD/install \
-	-DCMAKE_BUILD_TYPE=MinSizeRel \
+	-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:-Release} \
 	-DPython3_EXECUTABLE=$(which python3) \
 	-C $PWD/theos.cmake
 ninja -C build install/strip
